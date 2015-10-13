f667de55c33fe881d0547d6bf1f5b714950f68a5
shuangya <896640547@qq.com>
2015-10-13 23:01:17 +0800

    Add.自动识别语音

--- framework/base/i18n.php
+++ framework/base/i18n.php
@@ -36,6 +36,57 @@ class i18n {
 		static::$language = require ($path);
 	}
 	/**
+	 * 获取浏览器语言
+	 * @access public
+	 * @return string
+	 */
+	public static function getBrowserLanguage() {
+		//Accept-Language: zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3
+		$accept = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+		$accept = explode(';', $accept);
+		$accept_arr = [];
+		$accept_first = '';
+		foreach ($accept as $v) {
+			if (substr($v, 0, 2) === 'q=') {
+				$comma = strpos($v, ',');
+				if ($comma === FALSE) {
+					continue;
+				}
+				$q = intval(floatval(substr($v, 2, $comma - 2)) * 100);
+				$l = trim(substr($v, $comma + 1));
+				$accept_arr[$q] = explode(',', $l);
+			} elseif (empty($accept_first)) {
+				$accept_first = explode(',', trim($v));
+			} else {
+			}
+		}
+		krsort($accept_arr);
+		if (!empty($accept_first)) {
+			array_unshift($accept_arr, $accept_first);
+		}
+	}
+	/**
+	 * 获取默认语言
+	 * 优先级：浏览器发送的Accept-Language > Sy::$app[language]
+	 * @return string
+	 */
+	public static function getDefaultLanguage() {
+		$browser = static::getBrowserLanguage();
+		//对IE特殊照顾
+		$ie = ['zh-Hans-CN' => 'zh-CN', 'zh-Hans' => 'zh'];
+		if (count($browser) !== 0) {
+			$language = $browser[0][0];
+		} elseif (isset(Sy::$app['language'])) {
+			$language = Sy::$app['language'];
+		} else {
+			$language = 'en-US';
+		}
+		if (isset($ie[$language])) {
+			$language = $ie[$language];
+		}
+		return $language;
+	}
+	/**
 	 * 获得文字
 	 * @access public
 	 * @param string $key
@@ -44,7 +95,7 @@ class i18n {
 	 */
 	public static function get($key, $params = NULL) {
 		if (static::$language === NULL) {
-			static::setLanguage(Sy::$app['language']);
+			static::setLanguage(static::getDefaultLanguage());
 		}
 		if (!isset(static::$language[$key])) {
 			return '';
