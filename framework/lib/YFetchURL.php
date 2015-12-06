619799a2654df2f4a6ba5f76513b4911a9dcf403
shuangya <896640547@qq.com>
2015-12-07 00:42:37 +0800

    Add.YFetchURL自动执行

--- framework/lib/YFetchURL.php
+++ framework/lib/YFetchURL.php
@@ -18,32 +18,40 @@ use \Sy;
 class YFetchURL {
 	protected $handle;
 	protected $connectMethod;
+	protected $strExec = FALSE;
 	protected $defaultOpt = [
 		'returntransfer' => 1,
 		'header' => 1,
 		'ssl_verifypeer' => 0,
 		'ssl_verifyhost' => 0
 	];
-	protected $opt;
+	protected $opt = [];
 	static public function i($opt = []) {
 		return new self($opt);
 	}
-	public function __construct($opt) {
+	public function __construct($opt, $execute = TRUE) {
 		if (function_exists('curl_init')) {
 			$this->connectMethod = 'curl';
 		} else {
 			throw new SYException('Extension "curl" is required', '10050');
 		}
 		$this->init();
-		$this->opt = array_merge($this->defaultOpt, array_change_key_case($opt, CASE_LOWER));
+		$this->setopt(array_merge($this->defaultOpt, array_change_key_case($opt, CASE_LOWER)));
+		$this->strExec = $execute;
 	}
 	protected function init() {
 		if ($this->connectMethod === 'curl') {
 			$this->handle = curl_init();
 		}
 	}
+	public function setopt($opt) {
+		$this->opt = array_merge($this->opt, array_change_key_case($opt, CASE_LOWER));
+	}
 	public function exec() {
 		if ($this->connectMethod === 'curl') {
+			if (isset($this->opt['postfields']) && !isset($this->opt['customrequest'])) {
+				$this->opt['customrequest'] === 'POST';
+			}
 			if (isset($this->opt['customrequest'])) {
 				if ($this->opt['customrequest'] === 'GET') {
 				} elseif ($this->opt['customrequest'] === 'POST') {
@@ -67,7 +75,7 @@ class YFetchURL {
 			if ($this->opt['header'] == 1) {
 				return $this->parseResponse($response);
 			} else {
-				return ['type' => '', 'head' => [], 'body' => $response];
+				return $response;
 			}
 		}
 	}
@@ -93,6 +101,14 @@ class YFetchURL {
 			$head[trim($kk)] = trim($vv);
 		}
 		return ['type' => $responseType, 'head' => $head, 'body' => $body];
+	}
+	public function __toString() {
+		if ($this->strExec) {
+			$this->setopt(['header' => 0]);
+			return $this->exec();
+		} else {
+			return 'Object ' . __CLASS__ . '(' . strval($this->opt). ')';
+		}
 	}
 	public function __destruct() {
 		if ($this->connectMethod === 'curl' && $this->handle !== NULL) {
