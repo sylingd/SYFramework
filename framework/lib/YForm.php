b79a6b21975e116426f29349f1dcf9027794e364
shuangya <896640547@qq.com>
2015-12-14 00:01:37 +0800

    修改表单验证

--- framework/lib/YForm.php
+++ framework/lib/YForm.php
@@ -15,7 +15,6 @@ namespace sy\lib;
 use \Sy;
 
 class YForm {
-	public static $validString = NULL;
 	/**
 	 * 格式验证
 	 * @param string $str 待验证字符串
@@ -23,18 +22,76 @@ class YForm {
 	 * @return boolean
 	 */
 	public static function valid($str, $type) {
-		if (static::$validString === NULL) {
-			static::$validString = require (SY_ROOT . 'data/validString.php');
-		}
-		if (isset(static::$validString[$type])) {
-			$m = static::$validString[$type];
-			if (is_string($m)) {
-				return preg_match($m, $str) ? TRUE : FALSE;
-			} else {
-				return $m($str);
-			}
+		$func = 'self::is' . ucfirst($type);
+		if (is_callable($func)) {
+			return call_user_func($func, $str);
 		} else {
 			return TRUE;
 		}
+	}
+	/**
+	 * 验证字符串是否为EMail
+	 * @access public
+	 * @param string $str
+	 * @return boolean
+	 */
+	public static function isEmail($str) {
+		return preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $str);
+	}
+	/**
+	 * 验证字符串是否为合法的IP地址
+	 * @access public
+	 * @param string $ip
+	 * @return boolean
+	 */
+	public static function isIpv4($ip) {
+		$ip = explode('.', $ip);
+		if (count($ip) !== 4) {
+			return FALSE;
+		}
+		foreach ($ip as $one) {
+			if ($one == '' || !is_int($one) || (int)$one > 255 || (int)$one < 0) {
+				return FALSE;
+			}
+		}
+		return TRUE;
+	}
+	public static function isIpv6($ip) {
+		$collapsed = FALSE;
+		$chunks = array_filter(preg_split('/(:{1,2})/', $str, NULL, PREG_SPLIT_DELIM_CAPTURE));
+		if (current($chunks) == ':' || end($chunks) == ':') {
+			return FALSE;
+		}
+		while ($seg = array_pop($chunks)) {
+			if ($seg[0] == ':') {
+				if (strlen($seg) > 2) {
+					return FALSE;
+				}
+				if ($seg == '::') {
+					if ($collapsed) {
+						return FALSE;
+					}
+					$collapsed = TRUE;
+				}
+			} elseif (preg_match("/[^0-9a-f]/i", $seg) || strlen($seg) > 4) {
+				return FALSE;
+			}
+		}
+		return $collapsed;
+	}
+	/**
+	 * 验证字符串是否为日期
+	 * @access public
+	 * @param string $date
+	 * @return boolean
+	 */
+	public static function isDate($date) {
+		if (!preg_match('/^(\d{4})-(\d{1,2})-(\d{1,2})$/', $date)) {
+			return FALSE;
+		}
+		if (strtotime($date) === FALSE) {
+			return FALSE;
+		}
+		return TRUE;
 	}
 }
