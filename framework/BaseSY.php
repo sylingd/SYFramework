3ad9c85e98f55744a99c31aa09d7e9d65b971e67
ShuangYa <896640547@qq.com>
2016-07-13 22:10:25 +0800

    Add swooleTask支持

--- framework/BaseSY.php
+++ framework/BaseSY.php
@@ -172,64 +172,9 @@ class BaseSY {
 			'worker_num' => static::$app['httpServer']['worker_num'],
 			'daemonize' => TRUE
 		]);
-		$serv->on('request', function($req, $response) {
-			//生成唯一请求ID
-			$remoteIp = ($req->server['remote_addr'] === '127.0.0.1' && isset($req->server['http_x_forwarded_for'])) ? $req->server['http_x_forwarded_for'] : $req->server['remote_addr']; //获取真实IP
-			$requestId = md5(uniqid($remoteIp, TRUE));
-			//设置请求信息
-			static::$httpRequest[$requestId] = $req;
-			static::$httpResponse[$requestId] = $response;
-			//是否启用CSRF验证
-			if (isset(static::$app['csrf']) && static::$app['csrf']) {
-				\sy\lib\YSecurity::csrfSetCookie($requestId);
-			}
-			//根据设置，分配重写规则
-			ob_start();
-			if (static::$app['rewrite']) {
-				//自定义规则
-				if (is_array(static::$app['rewriteParseRule'])) {
-					$matches = NULL;
-					foreach (static::$app['rewriteParseRule'] as $oneRule) {
-						if (preg_match($oneRule[0], static::$httpRequest[$requestId]->server['request_uri'], $matches)) {
-							$route = $oneRule[1];
-							$paramName = array_slice($oneRule, 2);
-							$param = [];
-							foreach ($paramName as $k => $v) {
-								$param[$v] = isset($matches[$k + 1]) ? $matches[$k + 1] : '';
-							}
-							//写入相关的环境变量
-							//合并至GET参数
-							static::$httpRequest[$requestId]->get = array_merge($param, (array)static::$httpRequest[$requestId]->get);
-							static::$httpRequest[$requestId]->server['query_string'] = http_build_query(static::$httpRequest[$requestId]->get);
-							static::$httpRequest[$requestId]->server['request_uri'] = static::$httpRequest[$requestId]->server['php_self'] . '?' . static::$httpRequest[$requestId]->server['query_string'];
-							break;
-						}
-					}
-				}
-				//没有匹配的重写规则
-				if (!isset($route)) {
-					$url = parse_url(static::$httpRequest[$requestId]->server['request_uri']);
-					$route = ltrim(preg_replace('/\.(\w+)$/', '', $url['path']), '/'); //去掉末尾的扩展名和开头的“/”符号
-					empty($route) && $route = NULL;
-				}
-				static::router($route, $requestId);
-			} else {
-				$route = empty(static::$httpRequest[$requestId]->get[static::$routeParam]) ? static::$httpRequest[$requestId]->get[static::$routeParam] : NULL;
-				static::router($route, $requestId);
-			}
-			//请求结束，进行清理工作
-			try {
-				$output = ob_get_clean();
-				if (empty($output)) {
-					static::$httpResponse[$requestId]->end();
-				} else {
-					static::$httpResponse[$requestId]->end(ob_get_clean());
-				}
-				unset(static::$httpRequest[$requestId], static::$httpResponse[$requestId], $requestId);
-			} catch (\Exception $e) {
-			}
-			return;
-		});
+		$serv->on('request', ['\sy\swoole\serverEventHandle', 'onRequest']);
+		$serv->on('workerStart', ['\sy\swoole\serverEventHandle', 'onWorkerStart']);
+		$serv->on('task', ['\sy\swoole\serverEventHandle', 'onTask']);
 		$serv->start();
 	}
 	/**
