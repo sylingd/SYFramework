585c5946c34c0a20489de5773520e466b33d1448
ShuangYa <896640547@qq.com>
2016-07-30 18:11:35 +0800

    split a long function

--- framework/lib/YFetchURL.php
+++ framework/lib/YFetchURL.php
@@ -106,48 +106,7 @@ class YFetchURL {
 			$this->opt['customrequest'] = 'POST';
 		}
 		if ($this->connectMethod === 'curl') {
-			//处理请求方式
-			if (isset($this->opt['customrequest'])) {
-				$this->opt['customrequest'] = strtoupper($this->opt['customrequest']);
-				if ($this->opt['customrequest'] === 'GET') {
-				} elseif ($this->opt['customrequest'] === 'POST') {
-					curl_setopt($this->handle, CURLOPT_POST, 1);
-				} else {
-					curl_setopt($this->handle, CURLOPT_CUSTOMREQUEST, $this->opt['customrequest']);
-				}
-				unset($this->opt['customrequest']);
-			}
-			//处理timeout
-			if (isset($this->opt['timeout_ms'])) {
-				unset($this->opt['timeout']);
-			}
-			//处理POST数据
-			if (is_array($this->opt['postfields'])) {
-				if (count(array_filter($this->opt['postfields'], function($v) { return (strpos($v, '@') === 0); })) > 0) {
-					if (version_compare(PHP_VERSION, '5.6') >= 0 && is_array($this->opt['postfields'])) {
-						foreach ($this->opt['postfields'] as $k => $v) {
-							if (is_string($v) && strpos($v, '@') === 0) {
-								$this->opt['postfields'][$k] = new CURLFile(ltrim($v, '@'));
-							}
-						}
-					}
-				} else {
-					$this->opt['postfields'] = http_build_query($this->opt['postfields']);
-				}
-			}
-			//处理其他参数
-			foreach ($this->opt as $k => $v) {
-				if (defined('CURLOPT_' . strtoupper($k))) {
-					curl_setopt($this->handle, constant('CURLOPT_' . strtoupper($k)), $v);
-				}
-			}
-			curl_setopt($this->handle, CURLOPT_HTTPHEADER, $this->headerOutput($this->header));
-			//执行
-			$response = curl_exec($this->handle);
-			$errno = curl_errno($this->handle);
-			if ($errno > 0) {
-				throw new SYException(curl_error($this->handle), '10051');
-			}
+			$response = $this->_fetchCurl();
 			//将header和body分开
 			if ($this->opt['header'] == 1) {
 				return $this->parseResponse($response);
@@ -155,105 +114,168 @@ class YFetchURL {
 				return $response;
 			}
 		} elseif ($this->connectMethod === 'fsockopen') {
-			//处理timeout
-			if (isset($this->opt['timeout_ms'])) {
-				$this->opt['timeout'] = round($this->opt['timeout_ms'] / 1000);
+			$response = $this->_fetchSocket();
+			if ($this->opt['header'] == 1) {
+				return $response;
+			} else {
+				return $response['body'];
 			}
-			$start_time = time();
-			while (TRUE) {
-				//解析URL
-				$urlInfo = parse_url($this->opt['url']);
-				if ($urlInfo === FALSE) {
-					throw new SYException('Invalid URL', '10052');
+		}
+	}
+	/**
+	 * curl方式获取
+	 * @access protected
+	 * @return string
+	 */
+	protected function _fetchCurl() {
+		//处理请求方式
+		if (isset($this->opt['customrequest'])) {
+			$this->opt['customrequest'] = strtoupper($this->opt['customrequest']);
+			if ($this->opt['customrequest'] === 'GET') {
+			} elseif ($this->opt['customrequest'] === 'POST') {
+				curl_setopt($this->handle, CURLOPT_POST, 1);
+			} else {
+				curl_setopt($this->handle, CURLOPT_CUSTOMREQUEST, $this->opt['customrequest']);
+			}
+			unset($this->opt['customrequest']);
+		}
+		//处理timeout
+		if (isset($this->opt['timeout_ms'])) {
+			unset($this->opt['timeout']);
+		}
+		//处理POST数据
+		if (is_array($this->opt['postfields'])) {
+			if (count(array_filter($this->opt['postfields'], function($v) { return (strpos($v, '@') === 0); })) > 0) {
+				if (version_compare(PHP_VERSION, '5.6') >= 0 && is_array($this->opt['postfields'])) {
+					foreach ($this->opt['postfields'] as $k => $v) {
+						if (is_string($v) && strpos($v, '@') === 0) {
+							$this->opt['postfields'][$k] = new CURLFile(ltrim($v, '@'));
+						}
+					}
 				}
-				if ($urlInfo['scheme'] == 'https') {
-					$urlInfo['host'] = 'ssl://' . $urlInfo['host'];
-					$urlInfo['port'] = ($urlInfo['port'] != 0) ? $urlInfo['port'] : 443;
-				} else {
-					$urlInfo['port'] = ($urlInfo['port'] != 0) ? $urlInfo['port'] : 80;
-				}
-				$urlInfo['path'] = (isset($urlInfo['path']) ? $urlInfo['path'] : '/') . (isset($urlInfo['query']) ? '?' . $urlInfo['query'] : '');
-				$errorNumber = 0;
-				$errorString = NULL;
-				try {
-					$this->handle = fsockopen($urlInfo['host'], $urlInfo['port'], $errorNumber, $errorString, $this->opt['timeout'] - (time() - $start_time));
-					stream_set_timeout($this->handle, $this->opt['timeout'] - (time() - $start_time));
-				} catch (\Exception $e) {
-					throw new SYException($e->getMessage(), '10053');
-				}
-				if ($this->handle === FALSE) {
-					throw new SYException($errorString, '10053');
-				}
-				//header处理
-				if (isset($this->opt['customrequest'])) {
-					$this->opt['customrequest'] = strtoupper($this->opt['customrequest']);
-				} else {
-					$this->opt['customrequest'] = 'GET';
-				}
-				$request  = $this->opt['customrequest'] . ' ' . $urlInfo['path'] . ' HTTP/1.1' . "\r\n";
-				$request .= 'Host: ' . $urlInfo['host'] . "\r\n";
-				$request .= 'User-Agent: ' . $this->opt['useragent'] . "\r\n";
-				if ($this->opt['customrequest'] === 'POST') {
-					$queryString = '';
-					$isFile = FALSE;
-					if (is_array($this->opt['postfields'])) {
-						if (count(array_filter($this->opt['postfields'], function($v) { return (strpos($v, '@') === 0); })) > 0) {
-							$isFile = TRUE;
-							$boundary = substr(str_replace(' ', '', strval(microtime())), 2, 15);
-							foreach ($this->opt['postfields'] as $k => $v) {
-								$queryString .= '-----------------------------' . $boundary . "\r\n";
-								if (strpos($v, '@') === 0) {
-									//文件
-									$filename = substr($v, strrpos($v, '/') + 1);
-									$queryString .= 'Content-Disposition: form-data; name="' .$k . '"; filename="' . $filename .'"' . "\r\n";
-									$mimeType = Sy::getMimeType(pathinfo($filename, PATHINFO_EXTENSION));
-									is_null($mimeType) && $mimeType = 'application/octet-stream';
-									$queryString .= 'Content-Type: ' . $mimeType . "\r\n\r\n";
-									$queryString .= implode('', file(ltrim($v, '@')));
-									$queryString .= "\r\n" . '-----------------------------' . $boundary . "--\r\n";
-								} else {
-									//表单文本
-									$queryString .= 'Content-Disposition: form-data; name="' .$k . '"' . "\r\n";
-									$queryString .= rawurlencode($v) . "\r\n";
-								}
+			} else {
+				$this->opt['postfields'] = http_build_query($this->opt['postfields']);
+			}
+		}
+		//处理其他参数
+		foreach ($this->opt as $k => $v) {
+			if (defined('CURLOPT_' . strtoupper($k))) {
+				curl_setopt($this->handle, constant('CURLOPT_' . strtoupper($k)), $v);
+			}
+		}
+		curl_setopt($this->handle, CURLOPT_HTTPHEADER, $this->headerOutput($this->header));
+		//执行
+		$response = curl_exec($this->handle);
+		$errno = curl_errno($this->handle);
+		if ($errno > 0) {
+			throw new SYException(curl_error($this->handle), '10051');
+		}
+		return $response;
+	}
+	/**
+	 * fsocket方式抓取
+	 * @access protected
+	 * @return array
+	 */
+	protected function _fetchSocket() {
+		//处理timeout
+		if (isset($this->opt['timeout_ms'])) {
+			$this->opt['timeout'] = round($this->opt['timeout_ms'] / 1000);
+		}
+		$start_time = time();
+		while (TRUE) {
+			//解析URL
+			$urlInfo = parse_url($this->opt['url']);
+			if ($urlInfo === FALSE) {
+				throw new SYException('Invalid URL', '10052');
+			}
+			if ($urlInfo['scheme'] == 'https') {
+				$urlInfo['host'] = 'ssl://' . $urlInfo['host'];
+				$urlInfo['port'] = ($urlInfo['port'] != 0) ? $urlInfo['port'] : 443;
+			} else {
+				$urlInfo['port'] = ($urlInfo['port'] != 0) ? $urlInfo['port'] : 80;
+			}
+			$urlInfo['path'] = (isset($urlInfo['path']) ? $urlInfo['path'] : '/') . (isset($urlInfo['query']) ? '?' . $urlInfo['query'] : '');
+			$errorNumber = 0;
+			$errorString = NULL;
+			try {
+				$this->handle = fsockopen($urlInfo['host'], $urlInfo['port'], $errorNumber, $errorString, $this->opt['timeout'] - (time() - $start_time));
+				stream_set_timeout($this->handle, $this->opt['timeout'] - (time() - $start_time));
+			} catch (\Exception $e) {
+				throw new SYException($e->getMessage(), '10053');
+			}
+			if ($this->handle === FALSE) {
+				throw new SYException($errorString, '10053');
+			}
+			//header处理
+			if (isset($this->opt['customrequest'])) {
+				$this->opt['customrequest'] = strtoupper($this->opt['customrequest']);
+			} else {
+				$this->opt['customrequest'] = 'GET';
+			}
+			$request  = $this->opt['customrequest'] . ' ' . $urlInfo['path'] . ' HTTP/1.1' . "\r\n";
+			$request .= 'Host: ' . $urlInfo['host'] . "\r\n";
+			$request .= 'User-Agent: ' . $this->opt['useragent'] . "\r\n";
+			if ($this->opt['customrequest'] === 'POST') {
+				$queryString = '';
+				$isFile = FALSE;
+				if (is_array($this->opt['postfields'])) {
+					if (count(array_filter($this->opt['postfields'], function($v) { return (strpos($v, '@') === 0); })) > 0) {
+						$isFile = TRUE;
+						$boundary = substr(str_replace(' ', '', strval(microtime())), 2, 15);
+						foreach ($this->opt['postfields'] as $k => $v) {
+							$queryString .= '-----------------------------' . $boundary . "\r\n";
+							if (strpos($v, '@') === 0) {
+								//文件
+								$filename = substr($v, strrpos($v, '/') + 1);
+								$queryString .= 'Content-Disposition: form-data; name="' .$k . '"; filename="' . $filename .'"' . "\r\n";
+								$mimeType = Sy::getMimeType(pathinfo($filename, PATHINFO_EXTENSION));
+								is_null($mimeType) && $mimeType = 'application/octet-stream';
+								$queryString .= 'Content-Type: ' . $mimeType . "\r\n\r\n";
+								$queryString .= implode('', file(ltrim($v, '@')));
+								$queryString .= "\r\n" . '-----------------------------' . $boundary . "--\r\n";
+							} else {
+								//表单文本
+								$queryString .= 'Content-Disposition: form-data; name="' .$k . '"' . "\r\n";
+								$queryString .= rawurlencode($v) . "\r\n";
 							}
-						} else {
-							$queryString = http_build_query($this->opt['postfields']);
 						}
 					} else {
-						$queryString = $this->opt['postfields'];
+						$queryString = http_build_query($this->opt['postfields']);
 					}
-					$request .= 'Content-Type: ' . ($isFile ? 'multipart/form-data; boundary=---------------------------' . $boundary : 'application/x-www-form-urlencoded') . "\r\n";
-					$request .= 'Content-Length: ' . strlen($queryString) . "\r\n";
-				}
-				if (isset($urlInfo['user'])) {
-					$request .= 'Authorization: Basic ' . base64_encode($urlInfo['user'] . ':' . $urlInfo['pass']) . "\r\n";
-				}
-				foreach ($this->header as $k => $v) {
-					$request .= $k . ': ' . $v . "\r\n";
-				}
-				$request .= "\r\n";
-				if ($this->opt['customrequest'] === 'POST') {
-					$request .= $queryString;
-				}
-				//执行
-				fwrite($this->handle, $request);
-				$response = '';
-				while (!feof($this->handle)) {
-					$response .= fread($this->handle, 512);
-				}
-				//超时错误
-				$info = stream_get_meta_data($this->handle);
-				if ($info['time_out']) {
-					throw new SYException('Connect timeout', '10054');
-				}
-				$response = $this->parseResponse($response);
-				//处理跳转
-				if (isset($response['head']['Location']) && $this->opt['followlocation']) {
-					$this->opt['url'] = $response['head']['Location'];
 				} else {
-					return $response;
+					$queryString = $this->opt['postfields'];
 				}
+				$request .= 'Content-Type: ' . ($isFile ? 'multipart/form-data; boundary=---------------------------' . $boundary : 'application/x-www-form-urlencoded') . "\r\n";
+				$request .= 'Content-Length: ' . strlen($queryString) . "\r\n";
+			}
+			if (isset($urlInfo['user'])) {
+				$request .= 'Authorization: Basic ' . base64_encode($urlInfo['user'] . ':' . $urlInfo['pass']) . "\r\n";
+			}
+			foreach ($this->header as $k => $v) {
+				$request .= $k . ': ' . $v . "\r\n";
+			}
+			$request .= "\r\n";
+			if ($this->opt['customrequest'] === 'POST') {
+				$request .= $queryString;
+			}
+			//执行
+			fwrite($this->handle, $request);
+			$response = '';
+			while (!feof($this->handle)) {
+				$response .= fread($this->handle, 512);
+			}
+			//超时错误
+			$info = stream_get_meta_data($this->handle);
+			if ($info['time_out']) {
+				throw new SYException('Connect timeout', '10054');
+			}
+			$response = $this->parseResponse($response);
+			//处理跳转
+			if (isset($response['head']['Location']) && $this->opt['followlocation']) {
+				$this->opt['url'] = $response['head']['Location'];
+			} else {
+				return $response;
 			}
 		}
 	}
