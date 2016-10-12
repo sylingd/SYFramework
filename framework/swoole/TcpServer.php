acb6eb79380ff49000f277bafe192e11e2e4fbed
ShuangYa <896640547@qq.com>
2016-10-12 10:45:29 +0800

    Add TCP&UDP event handle

---
+++ framework/swoole/TcpServer.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * TCP支持类
+ * Swoole版本需求：1.8.6+
+ * 仅当运行于HttpServer模式时支持
+ * 
+ * @author ShuangYa
+ * @package SYFramework
+ * @category Swoole
+ * @link http://www.sylingd.com/
+ * @copyright Copyright (c) 2015-2016 ShuangYa
+ * @license http://lab.sylingd.com/go.php?name=framework&type=license
+ */
+
+namespace sy\swoole;
+
+use \Sy;
+use \sy\base\SYException;
+
+class TcpServer {
+	public function eventReceive($server, int $fd, int $from_id, string $data) {
+		$info = $server->connection_info($fd);
+		$port = $info['server_port'];
+		if (isset(Server::$eventHandle['Receive'][$port]) && is_callable(Server::$eventHandle['Receive'][$port])) {
+			call_user_func(Server::$eventHandle['Receive'][$port], $serv, $fd, $from_id, $data);
+		}
+	}
+}
