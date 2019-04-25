f289f899df35f3812c93c9fcf2a83339f9bf9b21
ShuangYa <896640547@qq.com>
2019-04-25 22:39:23 +0800

    Update ci password

--- tests/App/Config.php
+++ tests/App/Config.php
@@ -20,7 +20,7 @@ return [
 			'host' => 'localhost',
 			'port' => 3306,
 			'user' => 'root',
-			'password' => 'root',
+			'password' => '123456',
 			'database' => 'test'
 		],
 		'memcached' => [
